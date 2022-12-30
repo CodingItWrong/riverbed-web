d3019670b85c796d1fe2b5d23cb3789700fd98b7
Josh Justice <me@codingitwrong.com>
2022-12-30 08:01:30 -0500

    Configure linting and formatting

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'plugin:testing-library/react',
+  ],
+  plugins: [
+    'import',
+    
+    'cypress',
+  ],
+  env: {'cypress/globals': true},
+  rules: {
+    'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    'no-duplicate-imports': 'error',
+    quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
+    'react/jsx-uses-react': 'off',
+    'react/no-unstable-nested-components': ['warn', {allowAsProps: true}], // allow function props that return components
+    'react/react-in-jsx-scope': 'off',
+    'sort-imports': [
+      'warn',
+      {ignoreDeclarationSort: true, ignoreMemberSort: false},
+    ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+  },
+  
+};
