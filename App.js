27a669ac2c8f9cd94292d0a27382b22a96fb8a85
Josh Justice <me@codingitwrong.com>
2023-01-11 16:23:34 -0500

    Refresh on app focus on mobile

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {StatusBar} from 'react-native';
+import {
+  QueryClient,
+  QueryClientProvider,
+  focusManager,
+} from '@tanstack/react-query';
+import {useEffect} from 'react';
+import {AppState, Platform, StatusBar} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import CardList from './src/screens/CardList';
 
 const queryClient = new QueryClient();
 
+function onAppStateChange(status) {
+  if (Platform.OS !== 'web') {
+    focusManager.setFocused(status === 'active');
+  }
+}
+
 export default function App() {
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onAppStateChange);
+
+    return () => subscription.remove();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
