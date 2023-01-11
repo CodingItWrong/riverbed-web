1abc22bad7a45d9b1cbeed896c5fb4f7d8ee930d
Josh Justice <me@codingitwrong.com>
2023-01-11 07:08:00 -0500

    Remove unused unit test

--- App.spec.js
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react-native';
-import App from './App';
-
-describe('App', () => {
-  it('renders a hello message', () => {
-    render(<App />);
-    expect(screen.getByText('Hello, React Native!')).toBeVisible();
-  });
-});
