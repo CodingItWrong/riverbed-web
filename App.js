e59c935ab18cb4154531d8d7df161fe85f3152eb
Josh Justice <me@codingitwrong.com>
2023-01-11 06:12:14 -0500

    Remove unused import

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-// SafeAreaView fails without React import in CLI for some reason
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
 import {SafeAreaView, StatusBar, Text} from 'react-native';
 
 export default function App() {
