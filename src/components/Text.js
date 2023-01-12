ef2d6d3eb06373721792b4253d94e245b7fd044c
Josh Justice <me@codingitwrong.com>
2023-01-12 07:52:16 -0500

    Wrap text

---
+++ src/components/Text.js
@@ -0,0 +1,5 @@
+import {Text as PaperText} from 'react-native-paper';
+
+export default function Text({children}) {
+  return <PaperText>{children}</PaperText>;
+}
--- src/screens/CardList.js
+++ src/screens/CardList.js
@@ -1,11 +1,11 @@
 import {useMutation, useQuery, useQueryClient} from '@tanstack/react-query';
 import {useState} from 'react';
 import {FlatList} from 'react-native';
-import {Text} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import api from '../api';
 import Button from '../components/Button';
 import Card from '../components/Card';
+import Text from '../components/Text';
 import TextField from '../components/TextField';
 import ScreenBackground from './ScreenBackground';
 
