f66f84d0c18619036200127d30a0d2a0cbb342ad
Josh Justice <me@codingitwrong.com>
2023-01-13 06:49:02 -0500

    Simplify test data

--- cypress/e2e/edit-card.cy.js
+++ cypress/e2e/edit-card.cy.js
@@ -1,94 +1,14 @@
 describe('edit cards', () => {
   it('allows editing cards', () => {
-    // TODO: restructure this test for less duplication here. Maybe only one card at a time
-    const cards = [
-      {
-        id: '1',
-        attributes: {
-          'field-values': {
-            Title: 'Final Fantasy 7',
-            Publisher: 'Square Enix',
-          },
+    const card = {
+      id: '1',
+      attributes: {
+        'field-values': {
+          Title: 'Final Fantasy 7',
+          Publisher: 'Square Enix',
         },
       },
-      {
-        id: '2',
-        attributes: {
-          'field-values': {
-            Title: 'Castlevania: Symphony of the Night',
-            Publisher: 'Konami',
-          },
-        },
-      },
-    ];
-    const cardsAfterUpdate = [
-      {
-        id: '1',
-        attributes: {
-          'field-values': {
-            Title: 'Chrono Trigger',
-            Publisher: 'Square Enix',
-          },
-        },
-      },
-      {
-        id: '2',
-        attributes: {
-          'field-values': {
-            Title: 'Castlevania: Symphony of the Night',
-            Publisher: 'Konami',
-          },
-        },
-      },
-    ];
-    const cardsAfterDelete = [
-      {
-        id: '1',
-        attributes: {
-          'field-values': {
-            Title: 'Chrono Trigger',
-            Publisher: 'Square Enix',
-          },
-        },
-      },
-    ];
-    const cardsAfterAdd = [
-      {
-        id: '1',
-        attributes: {
-          'field-values': {
-            Title: 'Chrono Trigger',
-            Publisher: 'Square Enix',
-          },
-        },
-      },
-      {
-        id: '3',
-        attributes: {
-          'field-values': {},
-        },
-      },
-    ];
-    const cardsAfterUpdateNew = [
-      {
-        id: '1',
-        attributes: {
-          'field-values': {
-            Title: 'Chrono Trigger',
-            Publisher: 'Square Enix',
-          },
-        },
-      },
-      {
-        id: '3',
-        attributes: {
-          'field-values': {
-            Title: 'Earthbound',
-          },
-        },
-      },
-    ];
-
+    };
     cy.intercept('GET', 'http://localhost:3000/fields', {
       data: [
         {
@@ -102,7 +22,7 @@ describe('edit cards', () => {
       ],
     });
     cy.intercept('GET', 'http://localhost:3000/cards', {
-      data: cards,
+      data: [card],
     });
     cy.intercept('POST', 'http://localhost:3000/cards', {
       data: {type: 'cards', id: '3', attributes: {'field-values': {}}},
@@ -112,29 +32,36 @@ describe('edit cards', () => {
       'updateCard1',
     );
     cy.intercept('PATCH', 'http://localhost:3000/cards/3', {success: true});
-    cy.intercept('DELETE', 'http://localhost:3000/cards/2', {success: true}).as(
-      'deleteCard2',
+    cy.intercept('DELETE', 'http://localhost:3000/cards/1', {success: true}).as(
+      'deleteCard1',
     );
 
     cy.visit('/');
 
-    // show detail
-    cy.contains(cards[0].attributes['field-values'].Title).click();
-    cy.get(`[value="${cards[0].attributes['field-values'].Publisher}"]`);
+    cy.log('SHOW DETAIL');
+    cy.contains(card.attributes['field-values'].Title).click();
+    cy.get(`[value="${card.attributes['field-values'].Publisher}"]`);
 
-    // hide detail
+    cy.log('HIDE DETAIL');
     cy.contains('Close').click();
-    cy.contains(cards[0].attributes['field-values'].Publisher).should(
-      'not.exist',
-    );
+    cy.contains(card.attributes['field-values'].Publisher).should('not.exist');
 
-    // edit card
+    cy.log('EDIT CARD');
+    const updatedTitle = 'Chrono Trigger';
+    const updatedCard = {
+      id: '1',
+      attributes: {
+        'field-values': {
+          Title: updatedTitle,
+          Publisher: 'Square Enix',
+        },
+      },
+    };
     cy.intercept('GET', 'http://localhost:3000/cards', {
-      data: cardsAfterUpdate,
+      data: [updatedCard],
     });
 
-    const updatedTitle = 'Chrono Trigger';
-    cy.contains(cards[0].attributes['field-values'].Title).click();
+    cy.contains(card.attributes['field-values'].Title).click();
     cy.get('[data-testid=text-input-Title]').clear().type(updatedTitle);
     cy.contains('Save').click();
 
@@ -145,36 +72,48 @@ describe('edit cards', () => {
           type: 'cards',
           id: '1',
           attributes: {
-            'field-values': {Title: 'Chrono Trigger', Publisher: 'Square Enix'},
+            'field-values': {Title: updatedTitle, Publisher: 'Square Enix'},
           },
         },
       });
 
-    cy.contains(cards[0].attributes['field-values'].Title).should('not.exist');
-    cy.contains(cards[0].attributes['field-values'].Publisher).should(
-      'not.exist',
-    );
+    cy.contains(card.attributes['field-values'].Title).should('not.exist');
+    cy.contains(card.attributes['field-values'].Publisher).should('not.exist');
     cy.contains(updatedTitle);
 
-    // delete card
+    cy.log('DELETE CARD');
     cy.intercept('GET', 'http://localhost:3000/cards', {
-      data: cardsAfterDelete,
+      data: [],
     });
 
-    cy.contains(cards[1].attributes['field-values'].Title).click();
+    cy.contains(updatedTitle).click();
     cy.contains('Delete').click();
-    cy.wait('@deleteCard2');
-    cy.contains(cards[1].attributes['field-values'].Title).should('not.exist');
+    cy.wait('@deleteCard1');
+    cy.contains(updatedTitle).should('not.exist');
 
-    // create card
+    cy.log('CREATE CARD');
+    const newCard = {
+      id: '3',
+      attributes: {
+        'field-values': {},
+      },
+    };
     cy.intercept('GET', 'http://localhost:3000/cards', {
-      data: cardsAfterAdd,
+      data: [newCard],
     });
     const newTitle = 'Earthbound';
     cy.contains('Add Card').click();
     cy.get('[data-testid=text-input-Title]').clear().type(newTitle);
+    const updatedNewCard = {
+      id: '3',
+      attributes: {
+        'field-values': {
+          Title: 'Earthbound',
+        },
+      },
+    };
     cy.intercept('GET', 'http://localhost:3000/cards', {
-      data: cardsAfterUpdateNew,
+      data: [updatedNewCard],
     });
 
     cy.contains('Save').click();
