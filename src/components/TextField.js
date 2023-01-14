71e8a201e097cbde225bce37b80007e3f4409f41
Josh Justice <me@codingitwrong.com>
2023-01-14 10:34:45 -0500

    Make text fields multiline

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -3,6 +3,7 @@ import {TextInput as PaperTextInput} from 'react-native-paper';
 export default function TextField({label, value, onChangeText, testID}) {
   return (
     <PaperTextInput
+      multiline
       label={label}
       testID={testID}
       value={value}
