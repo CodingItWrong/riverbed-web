57443423ed8f90866922f28350f768df6ffb116d
Josh Justice <me@codingitwrong.com>
2023-01-16 07:12:48 -0500

    Run all specs

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const {defineConfig} = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:19006',
+    experimentalRunAllSpecs: true,
   },
   chromeWebSecurity: false,
 });
