9456dddeaba5a8ef117e8049323c5c5ab0a161f1
Josh Justice <me@codingitwrong.com>
2023-01-16 07:30:10 -0500

    Display columns

--- src/screens/CardList.js
+++ src/screens/CardList.js
@@ -5,6 +5,8 @@ import {
   KeyboardAvoidingView,
   Platform,
   StyleSheet,
+  Text,
+  View,
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Button from '../components/Button';
@@ -13,17 +15,22 @@ import FieldDisplay from '../components/FieldDisplay';
 import FieldInput from '../components/FieldInput';
 import ScreenBackground from '../components/ScreenBackground';
 import {useCards} from '../data/cards';
+import {useColumns} from '../data/columns';
 import {useFields} from '../data/fields';
 
 export default function CardList() {
   const queryClient = useQueryClient();
   const fieldClient = useFields();
+  const columnClient = useColumns();
   const cardClient = useCards();
   const [selectedCardId, setSelectedCardId] = useState(null);
   const [fieldValues, setFieldValues] = useState(null);
 
   const {data: fields = []} = useQuery(['fields'], () =>
     fieldClient.all().then(resp => resp.data),
+  );
+  const {data: columns = []} = useQuery(['columns'], () =>
+    columnClient.all().then(resp => resp.data),
   );
   const {data: cards = []} = useQuery(['cards'], () =>
     cardClient.all().then(resp => resp.data),
@@ -92,55 +99,62 @@ export default function CardList() {
           behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
           style={styles.fullHeight}
         >
-          <FlatList
-            data={cards}
-            keyExtractor={card => card.id}
-            renderItem={({item: card}) => {
-              if (selectedCardId === card.id) {
-                const fieldsToShow = fields;
+          <View style={styles.board}>
+            {columns.map(column => (
+              <View key={column.id}>
+                <Text>{column.attributes.name}</Text>
+                <FlatList
+                  data={cards}
+                  keyExtractor={card => card.id}
+                  renderItem={({item: card}) => {
+                    if (selectedCardId === card.id) {
+                      const fieldsToShow = fields;
 
-                return (
-                  <Card
-                    key={card.id}
-                    buttons={
-                      <>
-                        <Button onPress={hideDetail}>Close</Button>
-                        <Button onPress={deleteCard}>Delete</Button>
-                        <Button primary onPress={updateCard}>
-                          Save
-                        </Button>
-                      </>
+                      return (
+                        <Card
+                          key={card.id}
+                          buttons={
+                            <>
+                              <Button onPress={hideDetail}>Close</Button>
+                              <Button onPress={deleteCard}>Delete</Button>
+                              <Button primary onPress={updateCard}>
+                                Save
+                              </Button>
+                            </>
+                          }
+                        >
+                          {fieldsToShow.map(field => (
+                            <FieldInput
+                              key={field.id}
+                              field={field}
+                              value={fieldValues[field.id]}
+                              setValue={value => setFieldValue(field.id, value)}
+                            />
+                          ))}
+                        </Card>
+                      );
+                    } else {
+                      const fieldsToShow = fields.filter(
+                        field => field.attributes['show-in-summary'],
+                      );
+
+                      return (
+                        <Card key={card.id} onPress={() => showDetail(card.id)}>
+                          {fieldsToShow.map(field => (
+                            <FieldDisplay
+                              key={field.id}
+                              field={field}
+                              value={card.attributes['field-values'][field.id]}
+                            />
+                          ))}
+                        </Card>
+                      );
                     }
-                  >
-                    {fieldsToShow.map(field => (
-                      <FieldInput
-                        key={field.id}
-                        field={field}
-                        value={fieldValues[field.id]}
-                        setValue={value => setFieldValue(field.id, value)}
-                      />
-                    ))}
-                  </Card>
-                );
-              } else {
-                const fieldsToShow = fields.filter(
-                  field => field.attributes['show-in-summary'],
-                );
-
-                return (
-                  <Card key={card.id} onPress={() => showDetail(card.id)}>
-                    {fieldsToShow.map(field => (
-                      <FieldDisplay
-                        key={field.id}
-                        field={field}
-                        value={card.attributes['field-values'][field.id]}
-                      />
-                    ))}
-                  </Card>
-                );
-              }
-            }}
-          />
+                  }}
+                />
+              </View>
+            ))}
+          </View>
         </KeyboardAvoidingView>
       </SafeAreaView>
     </ScreenBackground>
@@ -150,5 +164,8 @@ export default function CardList() {
 const styles = StyleSheet.create({
   fullHeight: {
     flex: 1,
+  },
+  board: {
+    flexDirection: 'row',
   },
 });
