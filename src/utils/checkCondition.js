b9fd1e846cc55c4f0072dea5a6a1d0c8214a14f7
Josh Justice <me@codingitwrong.com>
2023-01-22 19:51:45 -0500

    Fix handling of empty card display condition

--- src/utils/checkCondition.js
+++ src/utils/checkCondition.js
@@ -1,14 +1,15 @@
 import QUERIES from '../enums/queries';
 
 export default function checkCondition({card, condition}) {
-  if (!condition) {
+  const {field, query} = condition ?? {};
+  if (!field || !query) {
     return true;
   }
 
   const value = card.attributes['field-values'][condition.field];
-  const query = QUERIES[condition.query];
-  if (query) {
-    return query.match(value);
+  const queryObject = QUERIES[condition.query];
+  if (queryObject) {
+    return queryObject.match(value);
   } else {
     console.error(`unrecognized query for condition: ${condition.query}`);
   }
