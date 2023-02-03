2c86ff759af378f7445191c8321f58b88094d57f
Josh Justice <me@codingitwrong.com>
2023-02-03 15:27:58 -0500

    Inline function into private helper

--- src/utils/checkConditions.js
+++ src/utils/checkConditions.js
@@ -1,4 +1,4 @@
-import checkCondition from './checkCondition';
+import QUERIES from '../enums/queries';
 
 export default function checkConditions({card, conditions}) {
   if (!conditions) {
@@ -6,4 +6,19 @@ export default function checkConditions({card, conditions}) {
   }
 
   return conditions.every(condition => checkCondition({card, condition}));
+}
+
+function checkCondition({card, condition}) {
+  const {field, query} = condition ?? {};
+  if (!field || !query) {
+    return true;
+  }
+
+  const value = card.attributes['field-values'][condition.field];
+  const queryObject = QUERIES[condition.query];
+  if (queryObject) {
+    return queryObject.match(value);
+  } else {
+    console.error(`unrecognized query for condition: ${condition.query}`);
+  }
 }
