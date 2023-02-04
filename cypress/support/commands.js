73ff8010575b3a1a91be0bb20051aa247306115b
Josh Justice <me@codingitwrong.com>
2023-02-03 19:43:57 -0500

    Remove log in test util

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,7 +20,6 @@ Cypress.Commands.add('assertContentsOrder', (selector, values) => {
 });
 
 Cypress.Commands.add('assertTestIdOrder', (selector, values) => {
-  cy.get(selector).then(console.log);
   values.forEach((childSelector, i) => {
     cy.get(selector)
       .eq(i)
