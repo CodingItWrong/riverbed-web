af472a3c5887997195916477d203d3604b573bac
Josh Justice <me@codingitwrong.com>
2023-02-18 08:21:22 -0500

    Sort cards and groups by choice label

---
+++ src/utils/getSortValue.js
@@ -0,0 +1,18 @@
+import FIELD_DATA_TYPES from '../enums/fieldDataTypes';
+
+export default function getSortValue({value, dataType, options}) {
+  switch (dataType) {
+    case FIELD_DATA_TYPES.CHOICE.key:
+      // sort by the choice label, not uuid
+      return options.choices.find(c => c.id === value)?.label.trim();
+    case FIELD_DATA_TYPES.DATE.key:
+    case FIELD_DATA_TYPES.DATETIME.key:
+    case FIELD_DATA_TYPES.NUMBER.key:
+      // dates are stored as strings that sort lexicographically
+      return value;
+    case FIELD_DATA_TYPES.TEXT.key:
+      return value?.trim();
+    default:
+      return `ERROR: unknown data type ${dataType}`;
+  }
+}
