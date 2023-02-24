ce670905dfa562a7017e3177ff0e3cf5acd1ad1f
Josh Justice <me@codingitwrong.com>
2023-02-24 14:09:05 -0500

    Approach to consider: returning only a single cardGroups value?

--- cypress/e2e/unit/cardGroups.cy.js
+++ cypress/e2e/unit/cardGroups.cy.js
@@ -2,7 +2,7 @@ import {groupCards} from '../../../src/utils/cardGroups';
 
 describe('card groups', () => {
   it('returns no card groups if there are no cards', () => {
-    const {cardGroups} = groupCards([]);
+    const cardGroups = groupCards([]);
 
     expect(cardGroups).to.deep.equal([]);
   });
@@ -11,7 +11,7 @@ describe('card groups', () => {
     const columnCards = ['something'];
     const cardGrouping = {field: null, direction: null};
 
-    const {cardGroups} = groupCards(columnCards, cardGrouping);
+    const cardGroups = groupCards(columnCards, cardGrouping);
     expect(cardGroups).to.deep.equal([{value: null, data: columnCards}]);
   });
 });
