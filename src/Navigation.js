921357c6e75c65ac467ca8d513f70d33800e7bb0
Josh Justice <me@codingitwrong.com>
2023-02-25 13:20:54 -0500

    Revert "Temporarily change board list title"
    This reverts commit 8da03ba516ebd540791786bae8e2d01d343b3e75.

--- src/Navigation.js
+++ src/Navigation.js
@@ -36,7 +36,7 @@ const Boards = () => {
           <BoardStack.Screen
             name="BoardList"
             component={BoardList}
-            options={{title: 'My Boards!!'}}
+            options={{title: 'My Boards'}}
           />
           <BoardStack.Screen name="Board" component={Board} />
         </>
