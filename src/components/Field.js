f425b9fd90a636c84460ece874703599f7bea295
Josh Justice <me@codingitwrong.com>
2023-02-27 08:16:10 -0500

    Update field editor rendering to use lookup

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,12 +1,7 @@
 import {View} from 'react-native';
-import FIELD_DATA_TYPES from '../enums/fieldDataTypes';
 import formatValue from '../utils/formatValue';
 import Text from './Text';
-import choiceFieldDataType from './fieldTypes/choice';
-import dateFieldDataType from './fieldTypes/date';
-import dateTimeFieldDataType from './fieldTypes/dateTime';
-import numberFieldDataType from './fieldTypes/number';
-import textFieldDataType from './fieldTypes/text';
+import fieldTypes from './fieldTypes';
 
 export default function Field({
   field,
@@ -32,68 +27,20 @@ export default function Field({
     );
   }
 
-  switch (dataType) {
-    case FIELD_DATA_TYPES.CHOICE.key: {
-      const {EditorComponent} = choiceFieldDataType;
-      return (
-        <EditorComponent
-          field={field}
-          value={value ?? ''}
-          setValue={setValue}
-          disabled={disabled}
-          style={style}
-        />
-      );
-    }
-    case FIELD_DATA_TYPES.DATE.key: {
-      const {EditorComponent} = dateFieldDataType;
-      return (
-        <EditorComponent
-          field={field}
-          value={value ?? ''}
-          setValue={setValue}
-          disabled={disabled}
-          style={style}
-        />
-      );
-    }
-    case FIELD_DATA_TYPES.DATETIME.key: {
-      const {EditorComponent} = dateTimeFieldDataType;
-      return (
-        <EditorComponent
-          field={field}
-          value={value ?? ''}
-          setValue={setValue}
-          disabled={disabled}
-          style={style}
-        />
-      );
-    }
-    case FIELD_DATA_TYPES.NUMBER.key: {
-      const {EditorComponent} = numberFieldDataType;
-      return (
-        <EditorComponent
-          field={field}
-          value={value ?? ''}
-          setValue={setValue}
-          disabled={disabled}
-          style={style}
-        />
-      );
-    }
-    case FIELD_DATA_TYPES.TEXT.key: {
-      const {EditorComponent} = textFieldDataType;
-      return (
-        <EditorComponent
-          field={field}
-          value={value ?? ''}
-          setValue={setValue}
-          disabled={disabled}
-          style={style}
-        />
-      );
-    }
-    default:
-      return <Text>ERROR: unknown data type {dataType}</Text>;
+  const fieldType = fieldTypes[dataType];
+
+  if (!fieldType) {
+    return <Text>ERROR: unknown data type {dataType}</Text>;
   }
+
+  const {EditorComponent} = fieldType;
+  return (
+    <EditorComponent
+      field={field}
+      value={value ?? ''}
+      setValue={setValue}
+      disabled={disabled}
+      style={style}
+    />
+  );
 }
