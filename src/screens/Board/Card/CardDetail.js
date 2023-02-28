76bf94f36038894e89d2910496d7396a9921aca2
Josh Justice <me@codingitwrong.com>
2023-02-28 10:26:34 -0500

    Comment about unused component

--- src/screens/Board/Card/CardDetail.js
+++ src/screens/Board/Card/CardDetail.js
@@ -1,6 +1,8 @@
 import Card from '../../../components/Card';
 import EditCardForm from './EditCardForm';
 
+// unused
+// TODO: remove and move other components under screens/Card once confirmed we are using the modal card detail approach
 export default function CardDetail({card, board, onChange, onCancel, style}) {
   return (
     <Card key={card.id} style={style}>
