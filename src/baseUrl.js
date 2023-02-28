821dddfa9b2edbb6f447249d5820753451d5c113
Josh Justice <me@codingitwrong.com>
2023-02-28 09:19:07 -0500

    Use same base URL config for simulator and physical

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,14 +1,11 @@
+import {isDevice} from 'expo-device';
 import {Platform} from 'react-native';
 
-const LOCAL_IP = null;
+// used by iOS physical dev device
 // ifconfig | grep "inet "
-// const LOCAL_IP = '10.0.1.52';
+const LOCAL_IP = '192.168.1.136';
 
 function getBaseUrl() {
-  if (LOCAL_IP) {
-    return `http://${LOCAL_IP}:3000`;
-  }
-
   if (window.Cypress) {
     return 'http://cypressapi';
   }
@@ -16,6 +13,8 @@ function getBaseUrl() {
   if (__DEV__) {
     if (Platform.OS === 'android') {
       return 'http://10.0.2.2:3000';
+    } else if (Platform.OS === 'ios' && isDevice) {
+      return `http://${LOCAL_IP}:3000`;
     } else {
       return 'http://localhost:3000';
     }
