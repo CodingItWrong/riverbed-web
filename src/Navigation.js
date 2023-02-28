04b27d72252981a69b8a8101b28474c12dd8698b
Josh Justice <me@codingitwrong.com>
2023-02-28 10:05:30 -0500

    Temp 2

--- src/Navigation.js
+++ src/Navigation.js
@@ -39,7 +39,7 @@ const Boards = () => {
           <BoardStack.Screen
             name="BoardList"
             component={BoardList}
-            options={{title: 'My Boards!'}}
+            options={{title: 'My Boards 2/28'}}
           />
           <BoardStack.Screen name="Board" component={Board} />
           <BoardStack.Group
