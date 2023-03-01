76eb1127ca7d728b139677c7f250ba4001bec2a6
Josh Justice <me@codingitwrong.com>
2023-03-01 09:45:59 -0500

    Set up React Native Web Maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,14 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
-  return config;
+  return {
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        'react-native-maps': 'react-native-web-maps',
+      },
+    },
+  };
 };
