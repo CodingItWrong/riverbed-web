0494391a15afc65df733fd70d34a596b1e64bbb3
Josh Justice <me@codingitwrong.com>
2023-03-02 12:49:41 -0500

    Add display and toggling of favorite star

--- src/screens/BoardList/BoardList.js
+++ src/screens/BoardList/BoardList.js
@@ -1,7 +1,7 @@
 import {useLinkTo, useNavigation} from '@react-navigation/native';
 import sortBy from 'lodash.sortby';
 import {useCallback, useEffect} from 'react';
-import {FlatList, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Button from '../../components/Button';
 import Card from '../../components/Card';
@@ -11,8 +11,9 @@ import LoadingIndicator from '../../components/LoadingIndicator';
 import ScreenBackground from '../../components/ScreenBackground';
 import Text from '../../components/Text';
 import sharedStyles from '../../components/sharedStyles';
-import {useBoards, useCreateBoard} from '../../data/boards';
+import {useBoards, useCreateBoard, useUpdateBoard} from '../../data/boards';
 import {useToken} from '../../data/token';
+import dateTimeUtils from '../../utils/dateTimeUtils';
 
 export default function BoardList() {
   const {clearToken} = useToken();
@@ -69,7 +70,14 @@ export default function BoardList() {
                     onPress={() => goToBoard(board)}
                     style={sharedStyles.mt}
                   >
-                    <Text>{board.attributes.name ?? '(unnamed board)'}</Text>
+                    <View style={styles.boardCard}>
+                      <View style={styles.boardTitle}>
+                        <Text variant="titleLarge">
+                          {board.attributes.name ?? '(unnamed board)'}
+                        </Text>
+                      </View>
+                      <FavoriteButton board={board} />
+                    </View>
                   </Card>
                 )}
               />
@@ -90,3 +98,39 @@ export default function BoardList() {
     </ScreenBackground>
   );
 }
+
+function FavoriteButton({board, onToggleFavorite}) {
+  const {attributes} = board;
+  const isFavorite = Boolean(attributes['favorited-at']);
+
+  const {mutate: updateBoard} = useUpdateBoard(board);
+  const handleUpdateBoard = () => {
+    const newFavoritedAt = isFavorite
+      ? null
+      : dateTimeUtils.objectToServerString(new Date());
+    updateBoard({...attributes, 'favorited-at': newFavoritedAt});
+  };
+
+  return (
+    <IconButton
+      accessibilityLabel="Favorite"
+      icon={isFavorite ? 'star' : 'star-outline'}
+      iconColor={isFavorite ? 'orange' : 'gray'}
+      style={styles.favoriteStar}
+      onPress={handleUpdateBoard}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  boardCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  boardTitle: {
+    flex: 1,
+  },
+  favoriteStar: {
+    margin: 0,
+  },
+});
