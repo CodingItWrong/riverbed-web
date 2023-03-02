d50f42226528b4645bfc2a04620c175912716dd9
Josh Justice <me@codingitwrong.com>
2023-03-02 13:38:01 -0500

    Bump title

--- src/Navigation.js
+++ src/Navigation.js
@@ -82,7 +82,7 @@ const AppNav = () => {
           <AppStack.Screen
             name="BoardList"
             component={BoardList}
-            options={{title: 'My Boards 3/2 B'}}
+            options={{title: 'My Boards 3/2 C'}}
           />
           <AppStack.Screen
             name="BoardStack"
