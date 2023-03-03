8a3cc8b6d652b21698db3b6d9f09d5a8ac028499
Josh Justice <me@codingitwrong.com>
2023-03-03 16:34:11 -0500

    Correct approach for long-press-to-share

--- src/components/AutoDetectLink.js
+++ src/components/AutoDetectLink.js
@@ -1,6 +1,5 @@
 import {A} from '@expo/html-elements';
-import * as Sharing from 'expo-sharing';
-import {Platform, StyleSheet, View} from 'react-native';
+import {Share, StyleSheet, View} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import {isValidURL} from '../utils/urlUtils';
 import Text from './Text';
@@ -33,12 +32,8 @@ function preventParentClickBehavior(e) {
   e.stopPropagation();
 }
 
-const PLATFORM_SUPPORTS_URL_SHARING = Platform.OS !== 'android';
-
 async function share(url) {
-  if (PLATFORM_SUPPORTS_URL_SHARING && (await Sharing.isAvailableAsync())) {
-    return Sharing.shareAsync(url);
-  }
+  return Share.share({url});
 }
 
 const styles = StyleSheet.create({
