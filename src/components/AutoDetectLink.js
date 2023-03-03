1b5944f58914b1c802075a41dbf8a789cf854203
Josh Justice <me@codingitwrong.com>
2023-03-03 09:16:59 -0500

    Long press to share link

--- src/components/AutoDetectLink.js
+++ src/components/AutoDetectLink.js
@@ -1,5 +1,6 @@
 import {A} from '@expo/html-elements';
-import {StyleSheet} from 'react-native';
+import * as Sharing from 'expo-sharing';
+import {Platform, StyleSheet} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import {isValidURL} from '../utils/urlUtils';
 import Text from './Text';
@@ -9,16 +10,12 @@ export function AutoDetectLink({variant, children}) {
   const theme = useTheme();
 
   if (isValidURL(children)) {
-    function preventParentClickBehavior(e) {
-      // only seems necessary on web, but works on both
-      e.stopPropagation();
-    }
-
     return (
       <A
         href={children}
         target="_blank"
         onPress={preventParentClickBehavior}
+        onLongPress={() => share(children)}
         style={[styles.link, {color: theme.colors.primary}]}
       >
         {children}
@@ -26,6 +23,19 @@ export function AutoDetectLink({variant, children}) {
     );
   } else {
     return <Text variant={variant}>{children}</Text>;
+  }
+}
+
+function preventParentClickBehavior(e) {
+  // only seems necessary on web, but works on both
+  e.stopPropagation();
+}
+
+const PLATFORM_SUPPORTS_URL_SHARING = Platform.OS !== 'android';
+
+async function share(url) {
+  if (PLATFORM_SUPPORTS_URL_SHARING && (await Sharing.isAvailableAsync())) {
+    return Sharing.shareAsync(url);
   }
 }
 
