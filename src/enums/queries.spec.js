bc1571b4204156d36ff2396714b0d02896b1c61e
Josh Justice <me@codingitwrong.com>
2023-03-04 16:52:08 -0500

    Test IS_NOT_EMPTY

--- src/enums/queries.spec.js
+++ src/enums/queries.spec.js
@@ -1,13 +1,28 @@
 import QUERIES from './queries';
 
 describe('QUERIES', () => {
-  describe('IS_EMPTY', () => {
-    test.each(['hello', '0'])('returns false for %j', value => {
-      expect(QUERIES.IS_EMPTY.match(value)).toBe(false);
+  describe('EMPTY', () => {
+    const EMPTY_VALUES = ['', null, undefined];
+    const NOT_EMPTY_VALUES = ['hello', '0'];
+
+    describe('IS_EMPTY', () => {
+      test.each(EMPTY_VALUES)('returns true for %j', value => {
+        expect(QUERIES.IS_EMPTY.match(value)).toBe(true);
+      });
+
+      test.each(NOT_EMPTY_VALUES)('returns false for %j', value => {
+        expect(QUERIES.IS_EMPTY.match(value)).toBe(false);
+      });
     });
 
-    test.each(['', null, undefined])('returns true for %j', value => {
-      expect(QUERIES.IS_EMPTY.match(value)).toBe(true);
+    describe('IS_NOT_EMPTY', () => {
+      test.each(EMPTY_VALUES)('returns true for %j', value => {
+        expect(QUERIES.IS_NOT_EMPTY.match(value)).toBe(false);
+      });
+
+      test.each(NOT_EMPTY_VALUES)('returns false for %j', value => {
+        expect(QUERIES.IS_NOT_EMPTY.match(value)).toBe(true);
+      });
     });
   });
 });
