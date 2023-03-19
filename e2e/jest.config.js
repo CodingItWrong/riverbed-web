386bdf415c68d1be444defda5101a05480ef2057
Josh Justice <me@codingitwrong.com>
2023-03-19 06:56:43 -0400

    Change Detox tests to use default test.js extension

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   rootDir: '..',
-  testMatch: ['<rootDir>/e2e/**/*.e2e.js'],
+  testMatch: ['<rootDir>/e2e/**/*.test.js'],
   testTimeout: 120000,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
