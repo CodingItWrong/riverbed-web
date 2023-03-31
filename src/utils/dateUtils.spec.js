0016e6f775264c38381a746e72523e742eb87f15
Josh Justice <me@codingitwrong.com>
2023-03-31 07:13:50 -0400

    Test isMonthOffset and isCurrentMonth

--- src/utils/dateUtils.spec.js
+++ src/utils/dateUtils.spec.js
@@ -1,4 +1,7 @@
+import nowFn from '../utils/now';
 import dateUtils from './dateUtils';
+
+jest.mock('../utils/now');
 
 describe('dateUtils', () => {
   function getComponents(date) {
@@ -35,5 +38,63 @@ describe('dateUtils', () => {
       const result = dateUtils.addDays(date, -1);
       expect(getComponents(result)).toEqual([2023, 0, 31]);
     });
+  });
+
+  describe('isCurrentMonth', () => {
+    const now = new Date('2022-03-04T00:00:00.000Z');
+    const CASES = [
+      ['2022-03-01', true],
+      ['2022-03-04', true],
+      ['2022-03-05', true],
+      ['2022-03-31', true],
+      ['2022-04-01', false],
+      ['2022-02-28', false],
+      ['2021-03-04', false],
+      ['2023-03-04', false],
+      ['not a date', false],
+      ['', false],
+      [null, false],
+      [undefined, false],
+    ];
+
+    beforeEach(() => {
+      nowFn.mockReturnValue(now);
+    });
+
+    test.each(CASES)(
+      `when testing %j at ${now} returns %j`,
+      (value, result) => {
+        expect(dateUtils.isCurrentMonth(value)).toBe(result);
+      },
+    );
+  });
+
+  describe('isMonthOffset', () => {
+    const now = new Date('2022-03-04T00:00:00.000Z');
+    const CASES = [
+      ['2022-03-01', false],
+      ['2022-03-04', false],
+      ['2022-03-31', false],
+      ['2022-04-01', false],
+      ['2022-02-01', true],
+      ['2022-02-28', true],
+      ['2021-02-04', false],
+      ['2023-02-04', false],
+      ['not a date', false],
+      ['', false],
+      [null, false],
+      [undefined, false],
+    ];
+
+    beforeEach(() => {
+      nowFn.mockReturnValue(now);
+    });
+
+    test.each(CASES)(
+      `when testing %j at ${now} returns %j`,
+      (value, result) => {
+        expect(dateUtils.isMonthOffset(value, -1)).toBe(result);
+      },
+    );
   });
 });
