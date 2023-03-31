66a206f3be4cb6303cf4df84d8ef2b1227b60211
Josh Justice <me@codingitwrong.com>
2023-03-31 07:02:22 -0400

    Test and simplify isValidURL

--- src/utils/urlUtils.js
+++ src/utils/urlUtils.js
@@ -2,5 +2,5 @@ const URL_REGEX = /^\w+:\/\/([^/]+\.\w+)/;
 
 export function isValidURL(string) {
   // new URL does not throw on React Native for invalid URLs
-  return string.match(URL_REGEX);
+  return URL_REGEX.test(string);
 }
