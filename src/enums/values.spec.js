8390dc1bf35b34636fabb36373f092e6363e8fe6
Josh Justice <me@codingitwrong.com>
2023-04-02 14:46:58 -0400

    Text existing VALUES.NOW code

---
+++ src/enums/values.spec.js
@@ -0,0 +1,26 @@
+import nowFn from '../utils/now';
+import FIELD_DATA_TYPES from './fieldDataTypes';
+import VALUES from './values';
+
+jest.mock('../utils/now');
+
+describe('VALUES', () => {
+  describe('NOW', () => {
+    const CASES = [
+      [FIELD_DATA_TYPES.DATE.key, '2022-03-04'],
+      [FIELD_DATA_TYPES.DATETIME.key, '2022-03-04T12:00:00.000Z'],
+    ];
+
+    const now = new Date('2022-03-04T12:00:00.000Z');
+    beforeEach(() => {
+      nowFn.mockReturnValue(now);
+    });
+
+    test.each(CASES)(
+      `when setting a %j field to NOW at ${now} returns %j`,
+      (fieldDataType, result) => {
+        expect(VALUES.NOW.call(fieldDataType)).toBe(result);
+      },
+    );
+  });
+});
