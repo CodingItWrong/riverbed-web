89638abd0e1d6d9c71c1f3ec96031c6bdd7ac19a
Josh Justice <me@codingitwrong.com>
2023-04-13 06:38:37 -0400

    Extract ModalScreenWrapper

---
+++ src/components/ModalScreenWrapper.js
@@ -0,0 +1,31 @@
+import {Platform, ScrollView} from 'react-native';
+import Card from './Card';
+import CenterModal from './CenterModal';
+import ScreenBackground from './ScreenBackground';
+
+/**
+ * Wrapper around a modal screen.
+ * Provides extra styling for web as we don't get it by default.
+ */
+export default function ModalScreenWrapper({children, closeModal}) {
+  return Platform.select({
+    web: (
+      <CenterModal onDismiss={closeModal}>
+        <Card style={styles.wrapperCard} contentStyle={styles.cardContent}>
+          <ScrollView>{children}</ScrollView>
+        </Card>
+      </CenterModal>
+    ),
+    default: <ScreenBackground>{children}</ScreenBackground>,
+  });
+}
+
+const styles = StyleSheet.create({
+  wrapperCard: {
+    marginTop: 8,
+    maxHeight: '90%',
+  },
+  cardContent: {
+    flex: 1,
+  },
+});
