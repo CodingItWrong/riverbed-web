825c05295c63641d30c7997d61ed132ed3fdd2ed
Josh Justice <me@codingitwrong.com>
2023-04-24 06:17:54 -0400

    Change share extension webhook url

--- assets/ios/shareExtension/Config.swift
+++ assets/ios/shareExtension/Config.swift
@@ -10,10 +10,10 @@ import Foundation
 
 enum Config {
     // dev
-    static let webhookURL = URL(string: "http://localhost:3000/custom/links")!
+    static let webhookURL = URL(string: "http://localhost:3000/shares")!
     static let apiKey = "KAhbB18D0QZBFXBCJKE4xJZYyaWkUTK25LWrqNMIXI2-FSqT5NgJpA-ermcllZG3s8mqioWkfZWNlUVwcOIGrw"
 
     // production
-//    static let webhookURL = URL(string: "https://ciw-list.herokuapp.com/custom/links")!
+//    static let webhookURL = URL(string: "https://ciw-list.herokuapp.com/shares")!
 //    static let apiKey = "fake_api_key"
 }
