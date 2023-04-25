6a4d68abe20083cd9cbc52b1b3033b3e136443b1
Josh Justice <me@codingitwrong.com>
2023-04-25 07:51:58 -0400

    Create and rename board

--- src/data/httpClient.fake.js
+++ src/data/httpClient.fake.js
@@ -8,32 +8,51 @@ function jsonApiData(data) {
   return {data: {data}};
 }
 
+const boards = [];
+
 class FakeHttpClient {
   async get(path) {
-    switch (path) {
-      case 'boards?':
-        return jsonApiData([board]);
-      case `boards/${board.id}?`:
-        return jsonApiData(board);
-      case `boards/${board.id}/elements?`:
-        return jsonApiData(elements);
-      case `boards/${board.id}/columns?`:
-        return jsonApiData(columns);
-      case `boards/${board.id}/cards?`:
-        return jsonApiData(cards);
-      default:
-        const message = `GET request not faked: ${path}`;
-        console.error(message);
-        throw new Error(message);
+    let match;
+    if (path === 'boards?') {
+      return jsonApiData(boards);
+    } else if ((match = path.match(/^boards\/(\d+)\?$/))) {
+      const id = match[1];
+      return jsonApiData(boards.find(b => b.id === id));
+    } else if (path.match(/^boards\/(\d+)\/elements\?$/)) {
+      return jsonApiData([]);
+    } else if (path.match(/^boards\/(\d+)\/columns\?$/)) {
+      return jsonApiData([]);
+    } else if (path.match(/^boards\/(\d+)\/cards\?$/)) {
+      return jsonApiData([]);
+    } else {
+      const message = `GET request not faked: ${path}`;
+      console.error(message);
+      throw new Error(message);
     }
   }
 
-  async post(path, body, {headers} = {}) {
+  async post(path, _body, {headers: _headers} = {}) {
     switch (path) {
       case 'oauth/token':
         return {
           data: {access_token: 'fake_access_token'},
         };
+
+      case 'boards?':
+        const newBoard = {
+          type: 'boards',
+          id: getId(),
+          attributes: {
+            name: null,
+            icon: null,
+            'color-theme': null,
+            'favorited-at': null,
+            options: {},
+          },
+        };
+        boards.push(newBoard);
+        return jsonApiData(newBoard);
+
       default:
         const message = `POST request not faked: ${path}`;
         console.error(message);
@@ -41,16 +60,20 @@ class FakeHttpClient {
     }
   }
 
-  async patch(path, body, {headers} = {}) {
-    switch (path) {
-      default:
-        const message = `PATCH request not faked: ${path}`;
-        console.error(message);
-        throw new Error(message);
+  async patch(path, body, {headers: _headers} = {}) {
+    if (path.match(/^boards\/(\d+)\?$/)) {
+      const {id, attributes} = body.data;
+      const board = boards.find(b => b.id === id);
+      board.attributes = attributes;
+      return jsonApiData(board);
+    } else {
+      const message = `PATCH request not faked: ${path}`;
+      console.error(message);
+      throw new Error(message);
     }
   }
 
-  async delete(path, body, {headers} = {}) {
+  async delete(path, body, {headers: _headers} = {}) {
     switch (path) {
       default:
         const message = `DELETE request not faked: ${path}`;
@@ -65,43 +88,3 @@ function getId() {
   idIncrementor += 1;
   return String(idIncrementor);
 }
-
-const board = {
-  type: 'boards',
-  id: getId(),
-  attributes: {name: 'Fake Board'},
-};
-
-const messageField = {
-  type: 'elements',
-  id: getId(),
-  attributes: {
-    'element-type': 'field',
-    'data-type': 'text',
-    name: 'Message',
-    'show-in-summary': true,
-    options: {},
-  },
-};
-
-const elements = [messageField];
-
-const columns = [
-  {
-    type: 'columns',
-    id: getId(),
-    attributes: {name: 'All Cards'},
-  },
-];
-
-const cards = [
-  {
-    type: 'cards',
-    id: getId(),
-    attributes: {
-      'field-values': {
-        [messageField.id]: 'Hello, Fake!',
-      },
-    },
-  },
-];
