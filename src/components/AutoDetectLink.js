4a56ee6a12a33167d2b4cef9e45e0d4cc94c1048
Josh Justice <me@codingitwrong.com>
2023-06-19 17:01:39 -0400

    Remove unneeded manual theming for link

--- src/components/AutoDetectLink.js
+++ src/components/AutoDetectLink.js
@@ -1,25 +1,15 @@
 import Link from '@mui/material/Link';
 import {StyleSheet, View} from 'react-native';
-import {useTheme} from 'react-native-paper';
 import {isValidUrl} from '../utils/urlUtils';
 import Text from './Text';
 
 // Pass text to this and if it is a valid URL it will be rendered as a link
 export function AutoDetectLink({link, enableLinking, variant, children}) {
-  const theme = useTheme();
-
   if (enableLinking && isValidUrl(link)) {
     return (
       <View style={styles.linkContainer}>
-        <Link
-          href={link}
-          target="_blank"
-          onClick={preventParentClickBehavior}
-          style={{color: theme.colors.primary}}
-        >
-          <Text variant={variant} style={{color: theme.colors.primary}}>
-            {children}
-          </Text>
+        <Link href={link} target="_blank" onClick={preventParentClickBehavior}>
+          <Text variant={variant}>{children}</Text>
         </Link>
       </View>
     );
