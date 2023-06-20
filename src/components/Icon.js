12da36bd3f7edbf75687c4b375bf3935193ecc95
Josh Justice <me@codingitwrong.com>
2023-06-20 08:19:01 -0400

    Remove paper references from icon component

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,15 +1,6 @@
-import {useTheme} from 'react-native-paper';
 import getMuiIcon from './getMuiIcon';
 
 export default function Icon({name, color, sx, style}) {
-  const theme = useTheme();
   const IconComponent = getMuiIcon(name);
-  console.log({name, IconComponent});
-  return (
-    <IconComponent
-      color={color ?? theme.colors.onPrimaryContainer}
-      sx={sx}
-      style={style}
-    />
-  );
+  return <IconComponent color={color ?? undefined} sx={sx} style={style} />;
 }
