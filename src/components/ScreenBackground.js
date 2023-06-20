a4275bf31521368a60ce77e9e2d60eeaa7aa6b52
Josh Justice <me@codingitwrong.com>
2023-06-20 08:26:39 -0400

    Comment about screen background conversion

--- src/components/ScreenBackground.js
+++ src/components/ScreenBackground.js
@@ -1,6 +1,7 @@
 import {View} from 'react-native';
 import {withTheme} from 'react-native-paper';
 
+// This will become just an empty View/div once we complete the Paper removal
 function ScreenBackground({theme, style, children}) {
   const baseStyle = {flex: 1, backgroundColor: theme.colors.background};
   return <View style={[baseStyle, style]}>{children}</View>;
