32aa9a5125672e6b4ec6b5c666a04e918d91336b
Josh Justice <me@codingitwrong.com>
2023-06-22 17:19:24 -0400

    Remove unused comment

--- webpack.common.js
+++ webpack.common.js
@@ -14,10 +14,6 @@ module.exports = {
       patterns: [{from: 'public', to: '.'}],
     }),
     // MiniCssExtractPlugin configured via child configs
-    // see https://stackoverflow.com/a/62297869/477480
-    // `process.env.NODE_ENV === 'production'` must be `true` for production
-    // builds to eliminate development checks and reduce build size. You may
-    // wish to include additional optimizations.
     new webpack.DefinePlugin({
       'process.env.RIVERBED_GOOGLE_MAPS_API_KEY': JSON.stringify(
         process.env.RIVERBED_GOOGLE_MAPS_API_KEY,
