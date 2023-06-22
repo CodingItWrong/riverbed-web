d0072f5c82725f692bdb104eadd4d6356a19d48c
Josh Justice <me@codingitwrong.com>
2023-06-22 17:49:05 -0400

    Fix wrong background color in section header in dark mode

--- src/components/FormGroup.js
+++ src/components/FormGroup.js
@@ -1,13 +1,13 @@
 import Divider from '@mui/material/Divider';
-import ListSubheader from '@mui/material/ListSubheader';
 import Stack from '@mui/material/Stack';
+import SectionHeader from './SectionHeader';
 
 export default function FormGroup({title, children}) {
   return (
     <>
       <Divider />
       <div>
-        <ListSubheader component="div">{title}</ListSubheader>
+        <SectionHeader>{title}</SectionHeader>
         <Stack>{children}</Stack>
       </div>
       <Divider />
