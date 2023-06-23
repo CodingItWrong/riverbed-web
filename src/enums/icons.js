10262d9f6b37bf4fe710a3542dd5dd9567b75d58
Josh Justice <me@codingitwrong.com>
2023-06-23 10:54:30 -0400

    Update internal icon names to more closely match MUI

--- src/enums/icons.js
+++ src/enums/icons.js
@@ -1,18 +1,18 @@
 // find more at https://pictogrammers.com/library/mdi/
 const ICONS = [
-  {key: 'baseball', label: 'Baseball', muiName: 'baseball'},
-  {key: 'bed', label: 'Bed', muiName: 'bed-king-outline'},
-  {key: 'book', label: 'Book', muiName: 'book-open-outline'},
-  {key: 'chart', label: 'Chart', muiName: 'chart-timeline'},
-  {key: 'checkbox', label: 'Checkbox', muiName: 'checkbox-outline'},
-  {key: 'food', label: 'Food', muiName: 'food'},
-  {key: 'gamepad', label: 'Gamepad', muiName: 'gamepad-variant'},
+  {key: 'baseball', label: 'Baseball', muiName: 'sports-baseball'},
+  {key: 'bed', label: 'Bed', muiName: 'bed'},
+  {key: 'book', label: 'Book', muiName: 'menu-book'},
+  {key: 'chart', label: 'Chart', muiName: 'view-timeline'},
+  {key: 'checkbox', label: 'Checkbox', muiName: 'check-box'},
+  {key: 'food', label: 'Food', muiName: 'fastfood'},
+  {key: 'gamepad', label: 'Gamepad', muiName: 'sports-esports'},
   {key: 'link', label: 'Link', muiName: 'link'},
-  {key: 'map-marker', label: 'Map Marker', muiName: 'map-marker'},
-  {key: 'medical-bag', label: 'Medical Bag', muiName: 'medical-bag'},
-  {key: 'scale', label: 'Scale', muiName: 'scale-bathroom'},
-  {key: 'television', label: 'Television', muiName: 'television'},
-  {key: 'tree', label: 'Tree', muiName: 'tree'},
+  {key: 'map-marker', label: 'Map Marker', muiName: 'place'},
+  {key: 'medical-bag', label: 'Medical Bag', muiName: 'medical-services'},
+  {key: 'scale', label: 'Scale', muiName: 'monitor-weight'},
+  {key: 'television', label: 'Television', muiName: 'tv'},
+  {key: 'tree', label: 'Tree', muiName: 'park'},
 ];
 
 export default ICONS;
