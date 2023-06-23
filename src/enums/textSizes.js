d034378ca9d837a1273c07c73c1e1ce311d78a82
Josh Justice <me@codingitwrong.com>
2023-06-23 08:57:23 -0400

    Update text size names

--- src/enums/textSizes.js
+++ src/enums/textSizes.js
@@ -1,10 +1,10 @@
 const TEXT_SIZES = [
-  {key: 1, label: 'Title Large', muiVariant: 'h4'},
-  {key: 2, label: 'Title Medium', muiVariant: 'h5'},
-  {key: 3, label: 'Title Small', muiVariant: 'h6'},
-  {key: 4, label: 'Body Large', muiVariant: 'body1'},
-  {key: 5, label: 'Body Medium', muiVariant: 'body2'},
-  {key: 6, label: 'Body Small', muiVariant: 'caption'},
+  {key: 1, label: 'Largest', muiVariant: 'h4'},
+  {key: 2, label: 'Larger', muiVariant: 'h5'},
+  {key: 3, label: 'Large', muiVariant: 'h6'},
+  {key: 4, label: 'Small', muiVariant: 'body1'},
+  {key: 5, label: 'Smaller', muiVariant: 'body2'},
+  {key: 6, label: 'Smallest', muiVariant: 'caption'},
 ];
 
 export default TEXT_SIZES;
