6ba0e8108b751d4dded525d09ca4520af639c397
Josh Justice <me@codingitwrong.com>
2023-06-24 06:44:08 -0400

    Use standard React eslint instead of RN

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,53 @@
 module.exports = {
-  root: true,
-  extends: ['@react-native-community'],
-  plugins: ['import', 'cypress'],
-  env: {'cypress/globals': true},
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', 'import', 'cypress'],
+  parser: '@babel/eslint-parser',
+  env: {
+    browser: true,
+    'cypress/globals': true,
+    es6: true,
+    node: true,
+  },
+  globals: {
+    __DEV__: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'arrow-body-style': ['error', 'as-needed'],
     'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
-    'jest/no-focused-tests': 'error',
-    'jest/valid-expect': 'off',
     'no-duplicate-imports': 'error',
+    'prettier/prettier': 'error',
     quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
     'react/jsx-uses-react': 'off',
     'react/no-unstable-nested-components': ['warn', {allowAsProps: true}], // allow function props that return components
+    'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'sort-imports': [
       'warn',
       {ignoreDeclarationSort: true, ignoreMemberSort: false},
     ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
   },
+  overrides: [
+    {
+      files: ['**/*.spec.js'],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'],
+      rules: {
+        'jest/no-commented-out-tests': 'off',
+        'jest/no-focused-tests': 'error',
+        'jest/valid-expect': 'off',
+      },
+    },
+  ],
 };
