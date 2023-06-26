b9e2db3517adf1b488229ae9ab1903d7cddac21c
Josh Justice <me@codingitwrong.com>
2023-06-26 06:47:45 -0600

    Support wrapping in button group

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -1,24 +1,14 @@
 import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Text from '@mui/material/Typography';
 import {primaryColors} from '../theme/primaryColors';
 import Icon from './Icon';
 import Stack from './Stack';
 
 export default function ButtonGroup({label, value, onChangeValue, options}) {
-  function handleChange(event, newValue) {
-    onChangeValue(newValue === EMPTY_VALUE ? null : newValue);
-  }
-
   return (
     <Stack>
       <Text variant="bodySmall">{label}</Text>
-      <ToggleButtonGroup
-        exclusive
-        value={value ?? EMPTY_VALUE}
-        onChange={handleChange}
-        sx={{flexWrap: 'wrap'}}
-      >
+      <div>
         {options.map(option => {
           const color = option.iconColor
             ? primaryColors[option.iconColor]
@@ -26,16 +16,15 @@ export default function ButtonGroup({label, value, onChangeValue, options}) {
           return (
             <ToggleButton
               key={option.key}
-              value={option.key ?? EMPTY_VALUE}
               aria-label={option.label}
+              selected={value === option.key}
+              onChange={() => onChangeValue(option.key)}
             >
               <Icon name={option.icon} sx={{color}} />
             </ToggleButton>
           );
         })}
-      </ToggleButtonGroup>
+      </div>
     </Stack>
   );
 }
-
-const EMPTY_VALUE = '__empty__';
