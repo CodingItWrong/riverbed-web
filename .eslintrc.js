fa94f80a4052c93cb32d0b185edac52a75f8c8be
Josh Justice <me@codingitwrong.com>
2024-03-15 07:05:35 -0400

    Do not run Prettier via ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['prettier', 'import', 'cypress'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['import', 'cypress'],
   parser: '@babel/eslint-parser',
   env: {
     browser: true,
@@ -27,7 +23,6 @@ module.exports = {
   rules: {
     'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
     'no-duplicate-imports': 'error',
-    'prettier/prettier': 'error',
     quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
     'react/jsx-uses-react': 'off',
     'react/no-unstable-nested-components': ['warn', {allowAsProps: true}], // allow function props that return components
