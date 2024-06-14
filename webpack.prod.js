e0b3b6f0ab1ecd17c7369da935b3ce247cdb1a5c
Josh Justice <josh.justice2@cfacorp.com>
2024-06-14 15:24:25 -0400

    Error if var missing

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const dotenvResult = require('dotenv').config({path: './.env.production'});
 
 if (dotenvResult.error) {
   throw dotenvResult.error;
+} else if (!dotenvResult.parsed.RIVERBED_GOOGLE_MAPS_API_KEY) {
+  throw new Error('Missing required envvar: RIVERBED_GOOGLE_MAPS_API_KEY');
 }
 
 const webpack = require('webpack');
